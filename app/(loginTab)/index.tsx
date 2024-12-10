5f8c307e4f0e33688de3fe1d803c994b85b27e17
matiaszo <tiaszuniga@gmail.com>
2024-12-10 14:57:48 -0300

    something

--- app/(loginTab)/index.tsx
+++ app/(loginTab)/index.tsx
@@ -1,15 +1,24 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import { Text, Image } from 'react-native';
+import asd from "@/assets/images/barberLogo.png"
 
 
 export default function HomeScreen() {
   return (
     <>
+    <View style={styles.project}>
+      <Image source={asd} width={100} height={100}/>
+    </View>
       hey
     </>
   );
 }
 
 const styles = StyleSheet.create({
- 
+  project: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20
+  }
 });
