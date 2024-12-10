5c7793d4cb7327c02027fbcf462d67901a27a52c
matiaszo <tiaszuniga@gmail.com>
2024-12-10 16:32:06 -0300

    login and register kind of done

--- app/(loginTab)/_layout.tsx
+++ app/(loginTab)/_layout.tsx
@@ -1,21 +1,14 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Text, Image } from 'react-native';
-import loginLogo from "@/assets/images/login.png";
-import passwordLogo from "@/assets/images/function.png";
-import functionLogo from "@/assets/images/password.png";
-
 
 export default function TabLayout() {
 
   return (
-    <>
-      {/* <Image src={require("@/assets/images/barberLogo.png")} width={30} height={30}/> */}
       <Tabs screenOptions={{tabBarActiveBackgroundColor: '#5b4734', tabBarInactiveBackgroundColor: '#5b4734'}}>
-        <Tabs.Screen name="index" options={{headerShown: false,tabBarShowLabel: false, tabBarIcon: () =>(<Image source={loginLogo} width={50} height={50}/>)}}></Tabs.Screen>
-        <Tabs.Screen name="register" options={{headerShown: false,tabBarShowLabel: false, tabBarIcon: () =>(<Image source={passwordLogo} width={50} height={50}/>)}}></Tabs.Screen>
-        <Tabs.Screen name="recpassword" options={{headerShown: false, tabBarShowLabel: false,tabBarIcon: ()  =>(<Image source={functionLogo} width={50} height={50}/>)}}></Tabs.Screen>
+        <Tabs.Screen name="index" options={{headerShown: false,tabBarShowLabel: false, tabBarIcon: () =>(<Image source={require("@/assets/images/login.png")} width={50} height={50}/>)}}></Tabs.Screen>
+        <Tabs.Screen name="register" options={{headerShown: false,tabBarShowLabel: false, tabBarIcon: () =>(<Image source={require("@/assets/images/function.png")} width={50} height={50}/>)}}></Tabs.Screen>
+        <Tabs.Screen name="recpassword" options={{headerShown: false, tabBarShowLabel: false,tabBarIcon: ()  =>(<Image source={require("@/assets/images/password.png")} width={50} height={50}/>)}}></Tabs.Screen>
       </Tabs>
-    </>
   );
 }
