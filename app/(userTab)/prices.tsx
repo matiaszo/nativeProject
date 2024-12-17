7efa72f75c10a5f10a194dbaa28146583ed2cf99
matiaszo <tiaszuniga@gmail.com>
2024-12-17 16:13:25 -0300

    prices finished

--- app/(userTab)/prices.tsx
+++ app/(userTab)/prices.tsx
@@ -4,38 +4,36 @@ import { Link } from "expo-router";
 import React from "react";
 import Header from "@/components/Header";
 import { FlatList } from "react-native"; 
+import Price from "@/components/Price";
 
 interface IData {
+  id: string
   service: string, 
   price: number
 }
 
 const data : IData[] = [
   {
+    id: "1",
     service: "Simple Trim",
     price: 12
   },
   {
+    id: "2",
     service: "Eyebrows and Nose",
     price: 10
   },
   {
+    id: "3",
     service: "Mullet",
     price: 15
   },
   {
+    id: "4",
     service: "Full Face Wax",
     price: 19
   }
 ]
-
-const renderItem = (data: IData) => {
-  return (
-    <View>
-      
-    </View>
-  )
-}
 
 
 export default function PriceScreen() {
@@ -44,8 +42,11 @@ export default function PriceScreen() {
         <Image source={require("@/assets/images/barber-line.png")} width={50} height={50}/>
         <View style={styles.mainContainer}>
             <Header textOne="Check" textTwo="our prices"/>
-            <FlatList data={data} renderItem={undefined}>
-            </FlatList>
+              <View style={styles.card}>
+                <Text style={{fontFamily: "eRegular", fontSize:16,marginLeft: 30}}>Service:</Text>
+                <Text style={{fontFamily: "eRegular",fontSize:16,marginRight: 30}}>Price:</Text>
+              </View>
+              <FlatList style={{width: "100%"}} data={data} renderItem={({item})=> <Price service={item.service} price={item.price}/>} keyExtractor={item => item.id}/>
         </View>
     </SafeAreaView>
   );
@@ -56,6 +57,18 @@ const styles = StyleSheet.create({
         backgroundColor: "#131316",
         flex: 1
     },
+    card:{
+      width: "100%",
+      height: 30,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      backgroundColor: "#f7f7f7",
+      borderRadius: 5,
+      margin: 2,
+      marginBottom: 5
+  },
     header: {
         color: "#FFFFFF",
         fontFamily: "lobster",
---
+++ components/Price.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text,StyleSheet } from "react-native";
+
+const Price = ({service, price}: {service: string, price: number}) =>{
+    return(
+        <View style={styles.card}>
+            <Text style={{fontFamily: "eRegular", marginLeft: 30}}>{service}</Text>
+            <Text style={{fontFamily: "eRegular", marginRight: 40}}>{price}</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    card:{
+        height: 30,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#f7f7f7",
+        borderRadius: 5,
+        margin: 5,
+    }
+})
+export default Price
